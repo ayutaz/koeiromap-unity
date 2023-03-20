d17b268b294333ea3e42976d53cff7f32ae84631
yousan <ka1357amnbpdr@gmail.com>
2023-03-21 03:39:44 +0900

    feat: package export in menu

---
+++ Assets/KoeiromapUnity/Editor/PackageExporter.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace KoeiromapUnity.Editor
+{
+    public class PackageExporter
+    {
+        public const string RootDirectory = "Assets/KoeiromapUnity/Scripts";
+        public const string FileName = "KoeiromapUnity";
+
+        /// <summary>
+        ///     パッケージの書き出し(エディタ上でのテスト用)
+        ///     メニュー 「Tools > Export Unitypackage Test」をクリックで実行
+        /// </summary>
+        [MenuItem("Tools/Export Unitypackage Test")]
+        public static void ExportTestOnEditor()
+        {
+            var exportPath = EditorUtility.SaveFilePanel
+            (
+                "保存先を選択",
+                Application.dataPath,
+                $"{FileName}.unitypackage",
+                "unitypackage"
+            );
+
+            CreatePackage(RootDirectory, exportPath);
+        }
+
+        /// <summary>
+        ///     パッケージの書き出し
+        ///     GitHub Actionsから呼ばれる
+        /// </summary>
+        public static void Export()
+        {
+            CreatePackage(RootDirectory, $"build/{FileName}.unitypackage");
+        }
+
+        public static void CreatePackage(string rootDirectory, string exportPath)
+        {
+            SafeCreateDirectory(exportPath);
+            var assetsPaths = GetAllAssetsAtPath(rootDirectory);
+            AssetDatabase.ExportPackage(assetsPaths, exportPath, ExportPackageOptions.Default);
+            Debug.Log(
+                $"Export complete: {Path.GetFullPath(exportPath)}\nExport below files:\n{string.Join("\n", assetsPaths)}");
+        }
+
+        public static DirectoryInfo SafeCreateDirectory(string path)
+        {
+            var diParent = Directory.GetParent(path);
+            if (diParent == null || Directory.Exists(diParent.FullName)) return null;
+            return Directory.CreateDirectory(diParent.FullName);
+        }
+
+        public static string[] GetAllAssetsAtPath(string root)
+        {
+            return Directory.GetFiles(root, "*", SearchOption.AllDirectories)
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Where(x => !x.EndsWith(".meta"))
+                .Where(x => x != ".DS_Store")
+                .ToArray();
+        }
+    }
+}
